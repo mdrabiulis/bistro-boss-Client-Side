c958bcf9c816fc9a196470b969a25f43e05006e1
mdrabiulis <mdrabiulis155@gmail.com>
2023-11-19 23:59:46 +0600

    Category

--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   );
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar fixed z-50 bg-black text-white bg-opacity-30 max-w-screen-xl">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import Router from "./Router/Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={Router}></RouterProvider>
+    </div>
   </React.StrictMode>
 );
