79622b403071009489647bea6d91617e3b032802
mdrabiulis <mdrabiulis155@gmail.com>
2023-11-19 21:02:28 +0600

    NavBar

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Share File/Footer/Footer";
+import Navbar from "../Home/Navbar/Navbar";
 
 const Layout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
